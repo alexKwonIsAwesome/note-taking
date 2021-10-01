8e0510b4b9d7249eb5dc4eb8e5b91c5936aab313
Alex Kwon <khwalex@gmail.com>
2021-10-01 23:21:00 +0900

    Add app rendering logic with state management

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log('Hello, World');
+import { createApp } from './app/App';
+
+const state = {
+  activeCategory: 'notes',
+  notes: [
+    {
+      title: 'Note Title',
+      content: 'Lorem ipsum dolor sit amet.',
+      isArchived: false,
+    },
+    {
+      title: 'Note Title',
+      content: 'Lorem ipsum dolor sit amet.',
+      isArchived: false,
+    },
+    {
+      title: 'Note Title',
+      content: 'Lorem ipsum dolor sit amet.',
+      isArchived: true,
+    },
+    {
+      title: 'Note Title',
+      content: 'Lorem ipsum dolor sit amet.',
+      isArchived: false,
+    },
+    {
+      title: 'Note Title',
+      content: 'Lorem ipsum dolor sit amet.',
+      isArchived: false,
+    },
+  ],
+};
+
+const render = () => {
+  requestAnimationFrame(() => {
+    const $root = document.querySelector('#root')!;
+
+    const $app = createApp({
+      activeCategory: state.activeCategory as 'notes' | 'archived',
+      onCategoryChange: (category) => {
+        state.activeCategory = category;
+        render();
+      },
+      notes: state.notes,
+    });
+
+    const $currentApp = $root.firstElementChild;
+    if ($currentApp) {
+      $currentApp.replaceWith($app);
+    } else {
+      $root.appendChild($app);
+    }
+  });
+};
+
+render();
